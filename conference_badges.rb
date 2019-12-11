7da2e05b9a16c514e32b41c64c2fb22436da242d
Ho Yin Keung <hykeung92@gmail.com>
2019-12-11 23:57:20 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,6 +14,6 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees).each { |badge| badge}
-  assign_rooms(attendees).each{ |badge| badge}
+  batch_badge_creator(attendees).each { |badge| puts badge}
+  assign_rooms(attendees).each{ |badge| puts badge}
 end
